4cd959b4c5ebe28d3640245b9a772a58f3537142
Al Davidson <apdavidson@gmail.com>
2023-05-10 17:06:27 +0100

    100% spec coverage

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,72 +1,68 @@
 require 'spec_helper'
 require 'rack/test'
 require 'json'
+require 'faraday'
+require 'webmock/rspec'
+
+require_relative '../app'
 
 RSpec.describe 'Forwarding service' do
   include Rack::Test::Methods
 
-  let(:app) { Sinatra::Application }
+  let(:app) { ContentStoreProxyApp }
   let(:primary_response_body) { 'Primary response body' }
   let(:secondary_response_body) { 'Secondary response body' }
+  let(:primary_response_status) { 200 }
+  let(:secondary_response_status) { 201 }
+  let(:primary_url) { ENV['PRIMARY_UPSTREAM'] + '/foo' }
+  let(:secondary_url) { ENV['SECONDARY_UPSTREAM'] + '/foo' }
 
   before(:each) do
-    ENV['UPSTREAM_SERVICE_1'] = 'http://localhost:8081'
-    ENV['UPSTREAM_SERVICE_2'] = 'http://localhost:8082'
-  end
+    ENV['PRIMARY_UPSTREAM'] = 'http://localhost:8081'
+    ENV['SECONDARY_UPSTREAM'] = 'http://localhost:8082'
 
-  describe 'POST requests' do
-    let(:primary_response_status) { 200 }
-    let(:secondary_response_status) { 201 }
+    stub_request(:get, primary_url).to_return(status: primary_response_status, body: primary_response_body)
+    stub_request(:get, secondary_url).to_return(status: secondary_response_status, body: secondary_response_body)
+  end
+  
+
+  describe 'GET requests' do
 
     it 'forwards the request to the primary upstream service' do
-      stub_request(:post, ENV['UPSTREAM_SERVICE_1']).to_return(status: primary_response_status, body: primary_response_body)
-
-      post '/foo'
+      get '/foo'
 
       expect(last_response.status).to eq(primary_response_status)
       expect(last_response.body).to eq(primary_response_body)
     end
 
-    it 'forwards the request to the secondary upstream service if the primary returns an error' do
-      stub_request(:post, ENV['UPSTREAM_SERVICE_1']).to_return(status: 500)
-      stub_request(:post, ENV['UPSTREAM_SERVICE_2']).to_return(status: secondary_response_status, body: secondary_response_body)
+    context 'when the primary returns an error' do
+      let(:primary_response_status) { 500 }
 
-      post '/foo'
+      it 'forwards the request to the secondary upstream service' do
+        get '/foo'
+        expect(a_request(:get, secondary_url)).to have_been_made.once
+      end
 
-      expect(last_response.status).to eq(secondary_response_status)
-      expect(last_response.body).to eq(secondary_response_body)
+      it 'still returns the primary status and body' do
+        get '/foo'
+        expect(last_response.status).to eq(primary_response_status)
+        expect(last_response.body).to eq(primary_response_body)
+      end
     end
 
-    it 'returns the primary response even if the secondary response returns an error' do
-      stub_request(:post, ENV['UPSTREAM_SERVICE_1']).to_return(status: primary_response_status, body: primary_response_body)
-      stub_request(:post, ENV['UPSTREAM_SERVICE_2']).to_return(status: 500)
+    context 'when the secondary returns an error' do
+      let(:secondary_response_status) { 500 }
 
-      post '/foo'
+      it 'forwards the request to the primary upstream service' do
+        get '/foo'
+        expect(a_request(:get, primary_url)).to have_been_made.once
+      end
 
-      expect(last_response.status).to eq(primary_response_status)
-      expect(last_response.body).to eq(primary_response_body)
-    end
-
-    it 'logs the status code, response time and body size of the two responses' do
-      allow_any_instance_of(Faraday::Adapter::Test::Stubs).to receive(:post).and_return(double(status: primary_response_status, body: primary_response_body), double(status: secondary_response_status, body: secondary_response_body))
-
-      expect_any_instance_of(Logger).to receive(:info).with(/Primary response \(#{primary_response_status}\) received in \d+\.\d+ seconds and \d+ bytes/)
-      expect_any_instance_of(Logger).to receive(:info).with(/Secondary response \(#{secondary_response_status}\) received in \d+\.\d+ seconds and \d+ bytes/)
-
-      post '/foo'
-    end
-
-    it 'logs an error if there is a problem forwarding the request' do
-      allow(Faraday).to receive(:new).and_raise(StandardError.new('Could not connect'))
-
-      expect_any_instance_of(Logger).to receive(:error).with(/Could not connect/)
-
-      post '/foo'
-    end
-
-    it 'logs the first difference in the response bodies if they differ' do
-      allow_any_instance_of(Faraday::Adapter::Test::Stubs).to receive(:post).and_return(double(status: primary_response_status, body: 'Primary response body'), double(status: secondary_response_status, body: 'Secondary response body'))
-
-      expect_any_instance_of(Logger).to receive(:info).with(/Primary response \(#{primary_response_status}\) differs from secondary response \(#{secondary_response}\)/)
+      it 'still returns the primary status and body' do
+        get '/foo'
+        expect(last_response.status).to eq(primary_response_status)
+        expect(last_response.body).to eq(primary_response_body)
+      end
     end
   end
+end
